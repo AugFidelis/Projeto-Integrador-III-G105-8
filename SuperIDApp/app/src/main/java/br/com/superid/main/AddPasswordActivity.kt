0d5de71eebfa839e8f86d25f4121f171b7b80198
AugFidelis <augustofidelis2003@gmail.com>
2025-04-24 09:41:48 -0300

    Criação da base do aplicativo
    Criação do projeto no Android Studio, e criação dos arquivos para as Activities do projeto.

---
+++ SuperIDApp/app/src/main/java/br/com/superid/main/AddPasswordActivity.kt
@@ -0,0 +1,47 @@
+package br.com.superid.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.superid.main.ui.theme.SuperIDTheme
+
+class AddPasswordActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperIDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting7(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting7(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview7() {
+    SuperIDTheme {
+        Greeting7("Android")
+    }
+}
