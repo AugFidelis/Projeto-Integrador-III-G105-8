1455bea4b43c48863ba27953a2c9daab400abdbd
AugFidelis <augustofidelis2003@gmail.com>
2025-05-01 18:09:42 -0300

    Criação da base da tela de boas-vindas
    Criação e desenvolvimento incompleto da tela de boas-vindas do aplicativo.

--- SuperIDApp/app/src/main/java/br/com/superid/user/WelcomeActivity.kt
+++ SuperIDApp/app/src/main/java/br/com/superid/user/WelcomeActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.superid.R
 import br.com.superid.user.ui.theme.SuperIDTheme
 
 class WelcomeActivity : ComponentActivity() {
@@ -19,29 +38,65 @@ class WelcomeActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperIDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SuperIDApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SuperIDTheme {
-        Greeting("Android")
+fun SuperIDApp() {
+    WelcomeScreen(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+
+@Composable
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(bottom = 40.dp, top = 90.dp)
+        ) {
+        Image(painter = painterResource(id = R.drawable.superid_logo),
+            contentDescription = "Logo SuperID",
+            colorFilter = ColorFilter.tint(Color.Black),
+            //colorFilter temporário, enquanto fundo escuro não é adicionado
+            //modifier = Modifier.padding(top = 20.dp)
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Card(modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .fillMaxWidth()
+            .height(350.dp)
+        ) {
+            Text(textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(20.dp),
+                text = "Com o SuperID, você armazena todas as suas senhas em um só lugar, de forma segura e acessível. Não se preocupe mais em lembrar dezenas de códigos diferentes.")
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Button(onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+            ) {
+                Text("Realizar Cadastro")
+            }
+
+            Button(onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+            ) {
+                Text("Fazer Login")
+            }
+        }
     }
 }
