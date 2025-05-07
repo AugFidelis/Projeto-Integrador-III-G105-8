610c8dfe1bb99192dec40dc0fd477856148f363e
AugFidelis <augustofidelis2003@gmail.com>
2025-05-07 18:10:33 -0300

    Criação da barra superior da tela e texto básico sobre os termos

--- SuperIDApp/app/src/main/java/br/com/superid/user/TermsOfUseActivity.kt
+++ SuperIDApp/app/src/main/java/br/com/superid/user/TermsOfUseActivity.kt
@@ -1,16 +1,42 @@
 package br.com.superid.user
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.superid.R
 import br.com.superid.user.ui.theme.SuperIDTheme
 
 class TermsOfUseActivity : ComponentActivity() {
@@ -19,29 +45,71 @@ class TermsOfUseActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperIDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TermsOfUseApp()
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun TermsOfUseApp() {
+    TermsOfUseScreen(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
     )
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview2() {
-    SuperIDTheme {
-        Greeting2("Android")
+fun TermsOfUseScreen(modifier: Modifier = Modifier) {
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    val context = LocalContext.current
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = Color.Black
+                ),
+                title = {
+                    Text("Termos de uso")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        Intent(context, WelcomeActivity::class.java).also{
+                            context.startActivity(it)
+                        }
+                    }) {
+                        Image(painter = painterResource(R.drawable.returnarrow),
+                            contentDescription = "Seta de retorno à tela anterior",
+                            colorFilter = ColorFilter.tint(Color.Black)
+                            )
+                    }
+                }
+            )
+        }
+    ){ innerPadding ->
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                //.padding(bottom = 40.dp, top = screenHeight*0.03f)
+                .fillMaxWidth()
+                .padding(innerPadding)
+        ) {
+            Text(text = "Por favor, leia os termos de uso do aplicativo antes de continuar:",
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center,
+                fontSize = (screenHeight * 0.03f).value.sp,
+                modifier = Modifier.padding(screenHeight*0.03f)
+            )
+
+
+
+
+        }
     }
+
+
 }
