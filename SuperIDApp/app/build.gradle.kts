320be33a40da772bda41a0aabfb4bfb186367fb1
AugFidelis <augustofidelis2003@gmail.com>
2025-06-01 11:42:39 -0300

    Alteração no funcionamento do scanner de QR Code

--- SuperIDApp/app/build.gradle.kts
+++ SuperIDApp/app/build.gradle.kts
@@ -70,10 +70,10 @@ dependencies {
     implementation(libs.firebase.firestore.ktx)
 
     // CameraX
-    implementation("androidx.camera:camera-core:1.3.0")
-    implementation("androidx.camera:camera-camera2:1.3.0")
-    implementation("androidx.camera:camera-lifecycle:1.3.0")
-    implementation("androidx.camera:camera-view:1.3.0")
+    implementation("androidx.camera:camera-core:1.4.2")
+    implementation("androidx.camera:camera-camera2:1.4.2")
+    implementation("androidx.camera:camera-lifecycle:1.4.2")
+    implementation("androidx.camera:camera-view:1.4.2")
 
     // Compose
     implementation("androidx.compose.ui:ui:1.5.1")
